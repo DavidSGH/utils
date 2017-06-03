887f691b936721f6a2d48c8c3a03df114868ae13
DavidSGH <david.sghz@live.com>
2017-06-02 21:33:19 -0300

    Update getDate.bat

--- batch/getDate.bat
+++ batch/getDate.bat
@@ -1,12 +1,6 @@
 @echo on
   SETLOCAL
   
-  For /f "tokens=1-4 delims=/:.,abdehijlmnorstuvwy " %%a in ("%date%") Do (set mydate=%%c%%b%%a)
-  For /f "tokens=1-3 delims=/:.,abdehijlmnorstuvwy " %%a in ("%TIME%") Do (
-      Set HH=%%a
-      Set mm=%%b
-      Set ss=%%c
-  )
-
-  if 1%HH% LSS 20 Set HH=0%HH%
-  Echo The date is: %mydate%_%HH%%mm%%ss%
+  For /f "delims=. " %%a in ('wmic os get localdatetime ^|find "20"') Do set mydate=%%a
+  set datetimef=%mydate:~0,8%_%mydate:~8,6%
+  echo %datetimef%
